469ab53682fe94c7ad76ba6e4a96edba1fab314a
iambinod <helloitsbinod@gmail.com>
2023-05-08 13:20:23 +0545

    feaT: added a Navbar UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { Route, Routes } from "react-router-dom";
+import Index from "./pages/Index";
+import Layout from "./components/Layout";
+
+const App = () => {
+  return (
+    <>
+      <div className="font-Poppins">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Index />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
+  );
+};
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,46 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      rose: colors.rose,
+      pink: colors.pink,
+      fuchsia: colors.fuchsia,
+      purple: colors.purple,
+      violet: colors.violet,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      lightBlue: colors.sky, // Only in Tailwind CSS <=v2.1
+      sky: colors.sky, // As of Tailwind CSS v2.2, `lightBlue` has been renamed to `sky`
+      cyan: colors.cyan,
+      teal: colors.teal,
+      emerald: colors.emerald,
+      green: colors.green,
+      lime: colors.lime,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      orange: colors.orange,
+      red: colors.red,
+      slate: colors.slate,
+      zinc: colors.zinc,
+      gray: colors.gray,
+      neutral: colors.slate,
+      stone: colors.stone,
+    },
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
+      dropShadow: {
+        drop: "0px 2px 16px 3px rgba(0, 0, 0, 0.04);",
+      },
+    },
   },
   plugins: [],
-}
+};
